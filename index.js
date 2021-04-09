260dc098f9f1e1b957cbd4e5db24a8f3a5a25112
gigi <guka.tgs@gmail.com>
2021-04-09 15:33:19 -0400

    added a screenshot

---
+++ index.js
@@ -0,0 +1,13 @@
+//import modules -> Employee subclasses, fs, inquirer, path, page-template
+//use path module to define the path to the output directory
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+//create an array to hold of the team members
+
+//init function
+
+//function to create a manager
+//prompt user with questions needed to satisfy the input for a manager object
+//.then statment
+//create a new instance of manager class
